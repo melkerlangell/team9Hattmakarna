56fcd83a9f60ac21989b97c9cd872455da6f4219
Melker Langell <75675951+melkerlangell@users.noreply.github.com>
2024-04-01 17:33:57 +0200

    test
    Test för att se om detta funkar från en annan dator

--- src/hattmakarna/Hattmakarna.java
+++ src/hattmakarna/Hattmakarna.java
@@ -15,7 +15,7 @@ public class Hattmakarna {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("test");
+        System.out.println("test123");
     }
     
 }
