1ab1d60374468b3b1e059abdc2d986f8d545a4b3
Melker Langell <75675951+melkerlangell@users.noreply.github.com>
2024-04-02 13:26:33 +0200

    ...

--- src/hattmakarna/Hattmakarna.java
+++ src/hattmakarna/Hattmakarna.java
@@ -15,7 +15,6 @@ public class Hattmakarna {
      */
     public static void main(String[] args) {
         // TODO code application logic here
- 
         System.out.println("test");
     }
     //KoalaKevin
