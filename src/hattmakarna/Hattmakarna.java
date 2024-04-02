e9c38d6479a6b1f73eae43ad736f5b676787e469
knemil <153902555+knemil@users.noreply.github.com>
2024-04-02 10:16:10 +0200

    sdg

--- src/hattmakarna/Hattmakarna.java
+++ src/hattmakarna/Hattmakarna.java
@@ -16,7 +16,7 @@ public class Hattmakarna {
     public static void main(String[] args) {
         // TODO code application logic here
  
-        System.out.println("gtyvuybyu");
+        System.out.println("adgadr");
     }
     
 }
