b6c38eea0e805a662ebc1da12f452bc9f4b4458f
noahallvall <146009156+noahallvall@users.noreply.github.com>
2024-04-02 10:09:53 +0200

    ...

--- src/hattmakarna/Hattmakarna.java
+++ src/hattmakarna/Hattmakarna.java
@@ -16,7 +16,7 @@ public class Hattmakarna {
     public static void main(String[] args) {
         // TODO code application logic here
  
-        System.out.println("ttest");
+        System.out.println("gtyvuybyu");
     }
     
 }
