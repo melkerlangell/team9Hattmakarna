c0b4d50998e320d5fd05feb857d5af0b2a90fede
KoalaKevin <152889995+KoalaKevin@users.noreply.github.com>
2024-04-04 11:08:37 +0200

    Sprint 1
    Förfrågningar

---
+++ src/hattmakarna/Förfrågning.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hattmakarna;
+
+/**
+ *
+ * @author adams
+ */
+public class Förfrågning {
+    private int id;
+   private String tyg;
+    public Förfrågning(int id, String tyg){
+        this.id = id;
+        this.tyg = tyg;
+    }
+}
---
+++ src/hattmakarna/FörfrågningsRegister.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hattmakarna;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author adams
+ */
+
+public class FörfrågningsRegister {
+    ArrayList<Förfrågning> förfrågningar = new ArrayList();
+    
+    public void läggTill(){
+        Förfrågning F1 = new Förfrågning(1, "jeans");
+        förfrågningar.add(F1);
+        Förfrågning F2 = new Förfrågning(2, "Bomull");
+        förfrågningar.add(F2);
+    }
+    public void seArray(){
+        System.out.println(förfrågningar);
+    }
+}
+
+  
