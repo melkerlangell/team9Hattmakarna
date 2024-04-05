920cf7c46976838344a9ec41b89f7a99b965a1a6
noahallvall <146009156+noahallvall@users.noreply.github.com>
2024-04-05 10:32:04 +0200

    Uppdaterade produkthanteraren

--- src/hattmakarna/ProduktHanterare.java
+++ src/hattmakarna/ProduktHanterare.java
@@ -5,33 +5,41 @@ import java.util.ArrayList;
 public class ProduktHanterare {
     
     // ArrayList to store hatt names
-    private ArrayList<Hatt> produktLista;
+    private ArrayList<HattMall> produktLista;
 
     // Constructor to initialize the ArrayList
     public ProduktHanterare() {
         produktLista = new ArrayList<>();
         
-        Hatt hatt1 = new Hatt("M", "Blå", "Ingen", "Läder");
-        Hatt hatt2 = new Hatt("S","Grå","Fjäder","bomull");
+        HattMall hatt1 = new HattMall("M", "Blå", "Ingen", "Läder");
+        HattMall hatt2 = new HattMall("S","Grå","Fjäder","bomull");
         addHatt(hatt1);
         addHatt(hatt2);
+        
+        HattMall updatedHatt = new HattMall("L", "Röd", "Band", "Silke");
+        produktLista.set(1, updatedHatt);
+        System.out.println("Uppdaterat hattObjekt: " + updatedHatt.getInformation());
     }
 
     // Method to add a hatt to the list
-    public void addHatt(Hatt hattObjekt) {
+    public void addHatt(HattMall hattObjekt) {
         produktLista.add(hattObjekt);
     }
     
-    //Denna funkar inte! Lättare att förklara live
-    public void taBortHatt(Hatt hattIndex){
-        produktLista.remove(hattIndex);
+    public void taBortHatt(HattMall enHatt){
+        for (int i=0; i<produktLista.size(); i++){
+            HattMall enHattIListan = produktLista.get(i);
+            if(enHatt == enHattIListan){
+                produktLista.remove(enHattIListan);
+                break;
+            }            
+        }
     }
     
     public void visaHattar(){
-        for (Hatt enHatt : produktLista){
+        for (HattMall enHatt : produktLista){
             System.out.println(enHatt.getInformation());
         }
 }
-    
-    
+      
 }
