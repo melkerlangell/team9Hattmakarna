5d6cb23d99e6a3cc4c1da2e09d612db7cd4eb21e
Melker Langell <75675951+melkerlangell@users.noreply.github.com>
2024-04-08 09:18:46 +0200

    GUI

--- src/hattmakarna/Inloggningssida.java
+++ src/hattmakarna/Inloggningssida.java
@@ -28,6 +28,7 @@ public class Inloggningssida extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
+        avslutaKnapp = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         startKnapp = new javax.swing.JButton();
 
@@ -38,13 +39,28 @@ public class Inloggningssida extends javax.swing.JFrame {
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/hattmakarna/otto och judith logo transparent ny.png"))); // NOI18N
 
+        avslutaKnapp.setBackground(new java.awt.Color(102, 102, 102));
+        avslutaKnapp.setForeground(new java.awt.Color(255, 255, 255));
+        avslutaKnapp.setText("Avsluta");
+        avslutaKnapp.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        avslutaKnapp.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                avslutaKnappActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(79, 79, 79)
-                .addComponent(jLabel2)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(79, 79, 79)
+                        .addComponent(jLabel2))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(avslutaKnapp, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(101, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -52,7 +68,9 @@ public class Inloggningssida extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(100, 100, 100)
                 .addComponent(jLabel2)
-                .addContainerGap(150, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 96, Short.MAX_VALUE)
+                .addComponent(avslutaKnapp, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         jLabel1.setFont(new java.awt.Font("Segoe UI Light", 0, 24)); // NOI18N
@@ -80,7 +98,7 @@ public class Inloggningssida extends javax.swing.JFrame {
                         .addGap(32, 32, 32))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(startKnapp, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(55, 55, 55))))
+                        .addGap(62, 62, 62))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -88,7 +106,7 @@ public class Inloggningssida extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addGap(165, 165, 165)
+                .addGap(164, 164, 164)
                 .addComponent(startKnapp, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -102,6 +120,11 @@ public class Inloggningssida extends javax.swing.JFrame {
         this.dispose();
         new Personalmeny().setVisible(true);
     }//GEN-LAST:event_startKnappActionPerformed
+
+    private void avslutaKnappActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_avslutaKnappActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_avslutaKnappActionPerformed
 
     /**
      * @param args the command line arguments
@@ -139,6 +162,7 @@ public class Inloggningssida extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton avslutaKnapp;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
