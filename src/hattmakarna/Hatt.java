e861c5372569af732588f4eeef47515a1bff56a7
RebeckaLewin <rebeckalewin@hotmail.com>
2024-04-09 10:16:11 +0200

    Uppdaterade hatt

--- src/hattmakarna/Hatt.java
+++ src/hattmakarna/Hatt.java
@@ -10,12 +10,10 @@ import java.io.Serializable;
  * @author rebec
  */
 public class Hatt extends HattMall implements Serializable {
-    private int hattID;
     private String beskrivning;
     
-    public Hatt(String benamning, String storlek, String farg, String accessoar, String material, int id, String beskrivning){
+    public Hatt(String benamning, String storlek, String farg, String accessoar, String material, String beskrivning){
         super(benamning, storlek, farg, accessoar, material);
-        hattID = id;
         this.beskrivning = beskrivning;
     }  
 }
