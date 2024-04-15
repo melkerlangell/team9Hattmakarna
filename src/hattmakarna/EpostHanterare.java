deddeaf80b16b49d9b7d5954a5efa05dd5e413c8
KoalaKevin <152889995+KoalaKevin@users.noreply.github.com>
2024-04-15 09:42:47 +0200

    push!

---
+++ src/hattmakarna/EpostHanterare.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hattmakarna;
+import javax.mail.Session;
+import javax.mail.Message;
+import javax.mail.Transport;
+/**
+ *
+ * @author adams
+ */
+public class EpostHanterare {
+    
+}
