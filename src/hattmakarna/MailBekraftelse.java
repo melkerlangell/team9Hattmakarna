ae280a2ce7b63df78e7f86c15a70138d362a014a
KoalaKevin <152889995+KoalaKevin@users.noreply.github.com>
2024-04-17 11:36:02 +0200

    Mailbrekg

--- src/hattmakarna/MailBekraftelse.java
+++ src/hattmakarna/MailBekraftelse.java
@@ -9,5 +9,46 @@ package hattmakarna;
  * @author adams
  */
 public class MailBekraftelse {
-    
+
+    private static String meddelande = "Din order har nu skickats";
+    private static String amne = "Order Skickats";
+    private static String epost = "";
+    private Förfrågning enFörfrågning;
+
+    public static void skicka() {
+
+        EpostHanterare.skickaEpost(epost, amne, meddelande);
+    }
+
+    public void taBortFörfrågning() {
+        int id = enFörfrågning.getId();
+        FörfrågningsRegister.taBort(id);
+    }
+
+    public  void test() {
+
+        {
+            
+
+            
+            Kund kund = enFörfrågning.getKund();
+            Hatt hatt = enFörfrågning.getHatt();
+           
+            if (kund != null) {
+                String namn = kund.getNamn();
+                String epost = kund.getEpost();
+                String material = hatt.getMaterial()
+                if (hatt != null) {
+                    System.out.println("Typ: " + hatt.getBenamning());
+                    System.out.println("Material: " + hatt.getMaterial());
+                } else {
+                    System.out.println("Kunden har ingen hatt.");
+                }
+            } else {
+                System.out.println("Kund is null");
+            }
+
+            System.out.println("---------");
+        }
+    }
 }
